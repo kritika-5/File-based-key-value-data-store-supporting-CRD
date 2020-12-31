138382a3cbd7297d0bf9b2ea6541aa95b4bdefc4
kritika-5 <70874884+kritika-5@users.noreply.github.com>
2020-12-31 18:44:32 +0530

    Update access_code.py

--- access_code.py
+++ access_code.py
@@ -1,23 +1,23 @@
-#run the MODULE of MAIN FILE and import mainfile as a library 
+#Execute the MODULE of MAIN FILE and import mainfile as a library 
 
-import source_code as d #importing the main file("source_code" is the name of the file I have used) as a library 
-
-
-d.create("Apple",56) #to create a key with key_name,value given and with no time-to-live property
+import source_code as K #importing the main file("source_code" is the name of the file I have used) as a library 
 
 
-d.create("Samsung",96,2500) #to create a key with key_name,value given and with time-to-live property value given(number of seconds)
+K.create("Germany",65) #to create a key with key_name,value given and with no time-to-live property
 
 
-d.read("Apple") #it returns the value of the respective key in Jasonobject format 'key_name:value'
+K.create("Spain",69,2500) #to create a key with key_name,value given and with time-to-live property value given(number of seconds)
 
-d.read("Samsung") #it returns the value of the respective key in Jasonobject format 'key_name:value'
 
-d.read("Apple") #it returns the value of the respective key in Jasonobject format if the TIME-TO-LIVE IS NOT EXPIRED else it returns an ERROR
+K.read("Spain") #it returns the value of the respective key in Jasonobject format 'key_name:value'
 
-d.create("Apple",75) #it returns an ERROR since the key_name already exists in the database 
+K.read("Germany") #it returns the value of the respective key in Jasonobject format 'key_name:value'
 
-d.delete("Apple") #it deletes the respective key and its value from the database(memory is also freed)
+K.read("Spain") #it returns the value of the respective key in Jasonobject format if the TIME-TO-LIVE IS NOT EXPIRED else it returns an ERROR
+
+K.create("Spain",57) #it returns an ERROR since the key_name already exists in the database 
+
+K.delete("Spain") #it deletes the respective key and its value from the database(memory is also freed)
 
 #we can access these using multiple threads like
 th1=Thread(target=(create or read or delete),args=(key_name,value,timeout)) #as per the operation
